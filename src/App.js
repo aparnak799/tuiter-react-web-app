ae61bd0226a9bb091006887aa567e8d078f59d04
Aparna Krishnan <aparna.k799@gmail.com>
2023-03-04 10:59:52 -0800

    Tuiter + challenge almost done - CSS left

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
-import {BrowserRouter} from "react-router-dom";
-import {Routes, Route} from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         <Routes>
-          <Route index
-                 element={<Labs/>}/>
-          <Route path="/hello"
-                 element={<HelloWorld/>}/>
-          <Route path="/tuiter"
-                 element={<Tuiter/>}/>
+          <Route index element={<Labs />} />
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/tuiter/*" element={<Tuiter />} />
         </Routes>
       </div>
     </BrowserRouter>
